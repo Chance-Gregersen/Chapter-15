64c80e267572bfbce04a9ecc36eba96e30898df6
Chance-Gregersen <125307864+Chance-Gregersen@users.noreply.github.com>
2025-01-29 13:40:01 -0700

    Add files via upload

---
+++ Exercise15_ANM.java
@@ -0,0 +1,17 @@
+
+
+import javafx.application.Application;
+
+  import javafx.stage.Stage;
+
+  public class Exercise15_ANM extends Application {
+    public static void main(String[] args) {
+      launch(args);
+  }
+
+
+@Override
+    public void start(Stage primaryStage) {
+
+    }
+  }
